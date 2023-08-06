e8977039beef91d910a4607a8b3b13eb7a0c6eb6
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-08-06 23:15:02 +0700

    fix: download kertas kerja

--- src/components/Audit/ModalExcel.js
+++ src/components/Audit/ModalExcel.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import * as XLSX from 'xlsx';
 import axios from 'axios';
 import { filterselection } from './apiModalExcel';
 
-const ModalExcel = ({requestor_audit, smonth, syear, smonth2, syear2, smonth3, syear3, req, req2, req3}) => {
-  const [selection, setSelection]= useState(false);
+const ModalExcel = ({ requestor_audit, smonth, syear, smonth2, syear2, smonth3, syear3, req, req2, req3 }) => {
+  const [selection, setSelection] = useState(false);
   const [reqAudit, setReqaudit] = useState([]);
   const [exporData, setExportData] = useState([]);
 
   useEffect(() => {
     setReqaudit(
       requestor_audit.map(data => {
-        return{
+        return {
           selection: data.selection,
           id_project: data.id_project,
           no_nodin_bo: data.no_nodin_bo,
@@ -24,9 +24,9 @@ const ModalExcel = ({requestor_audit, smonth, syear, smonth2, syear2, smonth3, s
     )
   }, [])
 
-  const handleSelection =async(projid,value) =>{
-    let val =(Number(value));
-    console.log('id',projid, 'value in boolean', value,"or",val)
+  const handleSelection = async (projid, value) => {
+    let val = (Number(value));
+    console.log('id', projid, 'value in boolean', value, "or", val)
     try {
       await axios.patch(`datasProject/${projid}`, {
         selection: val
@@ -37,19 +37,19 @@ const ModalExcel = ({requestor_audit, smonth, syear, smonth2, syear2, smonth3, s
 
   }
 
-  const dataProjectHandler = async() =>{
-    const data = await filterselection(req, req2, req3, smonth, syear, smonth2, syear2, smonth3, syear3);   
+  const dataProjectHandler = async () => {
+    const data = await filterselection(req, req2, req3, smonth, syear, smonth2, syear2, smonth3, syear3);
     setExportData(data);
     // console.log(data);
     var wb = XLSX.utils.book_new()
     var ws = XLSX.utils.json_to_sheet(data);
     XLSX.utils.book_append_sheet(wb, ws, "Project");
-    XLSX.utils.sheet_add_aoa(ws, 
+    XLSX.utils.sheet_add_aoa(ws,
       [["Nodin Number", "Nodin Title", "Date",
         "No Nodin RFS/RFI", "No Nodin RFC/ITR"
-    ]], 
+      ]],
       { origin: "A1" });
-      ws["!cols"] = [ { wch: 30 } ];
+    ws["!cols"] = [{ wch: 30 }];
 
     XLSX.writeFile(wb, "KertasKerja.xlsx");
   }
@@ -72,38 +72,38 @@ const ModalExcel = ({requestor_audit, smonth, syear, smonth2, syear2, smonth3, s
           </tr>
         </thead>
         <tbody>
-          {requestor_audit.map((audit, index)=>(
+          {requestor_audit.map((audit, index) => (
             <tr key={audit.id_project}>
-            <td>{index + 1}</td>
-            <td>{audit.no_nodin_bo}</td>
-            <td>{audit.subject_nodin_rfsrfi}</td>
-            <td>{audit.date_nodin_rfsrfi}</td>
-            <td>{audit.no_nodin_rfsrfi}</td>
-            <td>{audit.no_nodin_rfcitr}</td>
-            <td>
-              <input
-                type="checkbox"
-                checked={audit.selection}
-                onChange={
-                  (e)=>{
-                    let valCheck = e.target.checked;
-                    console.log(e.target.checked)
+              <td>{index + 1}</td>
+              <td>{audit.no_nodin_bo}</td>
+              <td>{audit.subject_nodin_rfsrfi}</td>
+              <td>{audit.date_nodin_rfsrfi}</td>
+              <td>{audit.no_nodin_rfsrfi}</td>
+              <td>{audit.no_nodin_rfcitr}</td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={audit.selection == 1 ? true : false}
+                  onChange={
+                    (e) => {
+                      let valCheck = e.target.checked;
+                      console.log(e.target.checked)
                       setReqaudit(
                         requestor_audit.map(data => {
-                            if(audit.id_project === data.id_project){
-                              data.selection = valCheck
-                            }
-                              return data
+                          if (audit.id_project === data.id_project) {
+                            data.selection = valCheck
+                          }
+                          return data
                         })
                       )
-                  handleSelection(audit.id_project,audit.selection);
-                }
-              }
-            /></td>
-          </tr>
+                      handleSelection(audit.id_project, audit.selection);
+                    }
+                  }
+                /></td>
+            </tr>
           ))}
         </tbody>
-      </table> 
+      </table>
     </div>
   )
 }
